b1dbf0f4077ecff67b78ed12c556c210a657c7a5
vivek singhal <viveksinghal.coder@gmail.com>
2022-08-09 02:01:24 +0530

    add first

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+//it will connect the application to index.html
+
+import App from './components/App';
+
+ReactDOM.render(<App />,document.getElementById('root'));
+//rendor app companent in index.html.
+
+
